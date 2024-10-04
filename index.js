f90c9524bc95380a3b582c97c71843400b3f5402
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-04 17:05:04 -0500

    questions moved to array, questions replaced with array references

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import fs from 'fs';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    'title',
-    'description',
-    'installation',
-    'usage',
-    ''
+    'What is the title?',
+    'What is the description?',
+    'What are the installation instructions?',
+    'What is the usage information?',
+    'What is the license?',
+    'What are the contributing guidelines?',
+    'What are the test instructions?',
+    'What is your GitHub username?',
+    'What is your email addresss?'
 ];
 
 // TODO: Create a function to write README file
@@ -28,48 +32,48 @@ function init() {
         .prompt([
             { // 0
                 type: 'input',
-                message: ('What is the title?'), // questions[0],
+                message: questions[0],
                 name: 'title',
             },
             { // 1
                 type: 'input',
-                message: ('What is the description?'), // questions[1],
+                message: questions[1],
                 name: 'description',
             },
             { //2
                 type: 'input',
-                message: ('What are the installation instructions?'), // questions[2],
+                message: questions[2],
                 name: 'installation',
             },
             { // 3
                 type: 'input',
-                message: ('What is the usage information?'), // questions[3],
+                message: questions[3],
                 name: 'usage',
             },
             { // 4
                 type: 'list',
-                message: ('What is the license?'),
-                choices: ['choice1', 'choice2', 'choice3', 'choice4'], // questions[4],
+                message: questions[4],
+                choices: ['choice1', 'choice2', 'choice3', 'choice4'],
                 name: 'license',
             },
             { // 5
                 type: 'input',
-                message: ('What are the contributing guidelines?'), // questions[5],
+                message: questions[5],
                 name: 'contributing',
             },
             { // 6
                 type: 'input',
-                message: ('What are the test instructions?'), // questions[6],
+                message: questions[6],
                 name: 'test',
             },
             { // 7
                 type: 'input',
-                message: ('What is your GitHub username?'), // questions[7],
+                message: questions[7],
                 name: 'gitHubUN',
             },
             { // 8
                 type: 'input',
-                message: ('What is your email addresss?'), // questions[8],
+                message: questions[8],
                 name: 'email',
             },
         ])
